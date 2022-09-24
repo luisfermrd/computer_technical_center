e88cfdff968b02277c000b74cf4d75dcf3c4eac0
luisfermrd <100250619+luisfermrd@users.noreply.github.com>
2022-09-24 11:11:45 -0500

    true

--- app/config/config.php
+++ app/config/config.php
@@ -5,8 +5,10 @@ define('BD_SERVIDOR','localhost');
 define('BD_USUARIO','root');
 define('BD_PASSWORD','');
 define('BD_SISTEMA','computer_technical_center');
-
+define('PRUEBA','PRUEBA XD');
+define('PRUEBA2','PRUEBA XD');
 
 //Url local
 $URL = 'http://localhost/computer_technical_center';
+$URL2 = 'http://localhost/computer_technical_center';
 ?>
